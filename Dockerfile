fafd2f403d104515e572864bf907c182cf0d687a
Nandan Chandrashekar <nandan.chandrashekar@harness.io>
2020-05-05 21:50:40 -0700

    added strace

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ FROM python:3.6-slim
 ADD . /home/harness/
 WORKDIR /home/harness/
 
+RUN apt-get update && apt-get install -y make wget protobuf-compiler \
+    build-essential ntp \
+    libssl-dev strace procps
 
 RUN pip install nltk==3.4.5
 RUN python -m nltk.downloader punkt
--- child_process.py
+++ child_process.py
@@ -1,13 +1,13 @@
 
 import nltk
 
-# try:
-#     print("finding tokenizers/punkt")
-#     nltk.data.find('tokenizers/punkt')
-# except LookupError:
-#     print(LookupError)
-#     nltk.download('punkt')
-nltk.download('punkt')
+try:
+    print("finding tokenizers/punkt")
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    print(LookupError)
+    nltk.download('punkt')
+# nltk.download('punkt')
 
 if __name__ == "__main__":
 
